a8186e0500466ec6ff01ee585f8fc9d5639ab60e
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-06-20 21:43:49 -0500

    init

---
+++ Utility/DatabaseConnectionStringBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace PIEAPI.Utility
+{
+    public class DatabaseConnectionStringBuilder
+    {
+        public static string GetSqlConnectionString(IConfiguration config)
+        {
+            return $"Host={config["PIEADatabaseConnection_Server"]};Username={config["PIEADatabaseConnection_Username"]};Password={config["PIEADatabaseConnection_Password"]};Database={config["PIEADatabaseConnection_Database"]}"; 
+        }
+    }
+}
