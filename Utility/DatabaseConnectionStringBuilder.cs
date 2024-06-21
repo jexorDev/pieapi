9c82afe9c20d2c5de15dc49ec3a019b97218ad44
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-06-20 23:20:01 -0500

    fix connection string config

--- Utility/DatabaseConnectionStringBuilder.cs
+++ Utility/DatabaseConnectionStringBuilder.cs
@@ -4,7 +4,7 @@
     {
         public static string GetSqlConnectionString(IConfiguration config)
         {
-            return $"Host={config["PIEADatabaseConnection_Server"]};Username={config["PIEADatabaseConnection_Username"]};Password={config["PIEADatabaseConnection_Password"]};Database={config["PIEADatabaseConnection_Database"]}"; 
+            return $"Host={config["PIEDatabaseConnection_Server"]};Username={config["PIEDatabaseConnection_Username"]};Password={config["PIEDatabaseConnection_Password"]};Database={config["PIEDatabaseConnection_Database"]}"; 
         }
     }
 }
